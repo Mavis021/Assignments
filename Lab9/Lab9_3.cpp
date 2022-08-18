7cea99266dc7e9b96cd159375b73f329413059f0
Mamata Maharjan <077BCT043.mamata@pcampus.edu.np>
2022-08-18 20:31:01 +0545

    Exception Handling

---
+++ Lab9/Lab9_3.cpp
@@ -0,0 +1,125 @@
+//Lab9_4.cpp
+//exception handeling 
+
+#include <iostream>
+
+using namespace std;
+
+
+class Exception
+{
+private:
+    bool check;
+public:
+    Exception();
+    Exception(bool c):check(c){}
+    
+
+    void displayError()
+    {
+        if(check == true)
+        cout << "THE STACK IS FULL." << endl;
+        else
+            cout << "THE STACK IS EMPTY" << endl;
+    }
+};
+
+
+template <class T>
+
+class Stack
+
+{
+
+private:
+
+    T st[3];
+
+    int top;
+
+public:
+
+    Stack();
+
+    void push(T var);
+
+    T pop();
+
+};
+
+template <class T>
+
+ Stack<T>::Stack()
+
+{
+
+    top = -1;
+
+}
+
+template <class T>
+
+void Stack<T>::push(T var)
+
+{
+    if (top >= 1)
+    {
+        throw Exception (true);
+    }
+    else
+    {
+        st[++top] = var;
+    }
+
+}
+
+template <class T>
+
+T Stack<T>::pop()
+
+{
+    if (top < 0)
+    {
+        throw Exception (false);
+    }
+    else
+    {
+        return st[top--];
+    }
+
+}
+
+int main()
+
+{
+
+    Stack<float> s1;
+    try
+    {
+        s1.push(111.1F);
+
+        s1.push(222.2F);
+        s1.push(222.2F);
+
+    }
+    catch (Exception e)
+    {
+        e.displayError();
+    }
+    
+    try
+    {
+        cout << "1 : " << s1.pop() << endl;
+
+        cout << "2 : " << s1.pop() << endl;
+
+        cout << "3 : " << s1.pop() << endl;
+    }
+    catch(Exception e)
+    {
+        e.displayError();
+    }
+
+    return 0;
+
+}
